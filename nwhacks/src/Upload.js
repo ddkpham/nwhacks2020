513097080d35409a1fb64e287aa8f85ece9eabaa
Affan <affank@sfu.ca>
2020-01-12 11:31:40 -0800

    forms created

--- nwhacks/src/Upload.js
+++ nwhacks/src/Upload.js
@@ -5,6 +5,10 @@ import axios from "axios";
 class Upload extends React.Component {
     state = {
         selectedFile : null,
+        drug_name: "",
+        max_pills: "",
+        curr_pills: "",
+        dosage: "",
 
     }
 
@@ -26,15 +30,43 @@ class Upload extends React.Component {
         });
     }
 
+    formChangeHandler = (e) => {
+        const vm = this;
+         vm.setState({
+            [e.target.name]: e.target.value
+         })
+    }
+
     render() {
         const vm = this;
+        let forms = formComponent(vm);
         return (
             <div className="App">
                 <input type="file" onChange={vm.fileSelectedHandler}/>
                 <button onClick={vm.fileUploadHandler}>Upload</button>
+                {forms}
             </div>
         )
     }
+}
+
+const formComponent = (vm) => {
+    return (
+        <form>
+            <div className="form-row">
+                <div className="col">
+                    <input name="drug_name" type="text" className="form-control" placeholder="Drug Name" value={vm.state.drug_name} onChange={vm.formChangeHandler}/>
+                </div>
+                <div className="col">
+                    <input name="max_pills" type="text" className="form-control" placeholder="Max Pills Per Day" value={vm.state.max_pills} onChange={vm.formChangeHandler}/>
+                </div>
+                <div className="col">
+                    <input name="dosage" type="text" className="form-control" placeholder="Dosage" value={vm.state.dosage} onChange={vm.formChangeHandler}/>
+                </div>
+            </div>
+        </form>
+
+    );
 }
 
 export default Upload;
